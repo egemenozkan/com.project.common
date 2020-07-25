9b8869c4dd0da2e9167564c0d3a219c85c014762
egemenozkan@gmail.com <egemenozkan@gmail.com>
2020-07-25 23:13:23 +0300

    merged

---
+++ src/main/java/com/project/common/model/BaseResponseModel.java
@@ -0,0 +1,37 @@
+package com.project.common.model;
+
+import java.io.Serializable;
+
+public class BaseResponseModel implements Serializable {
+	private static final long serialVersionUID = -1626350357015050829L;
+
+	private boolean success;
+	private String errorCode;
+	private String errorMessage;
+	
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+}
