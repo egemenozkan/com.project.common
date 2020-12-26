0fb60a766a90b12664946494e0c657b986c635bf
egemenozkan@gmail.com <egemenozkan@gmail.com>
2020-12-26 17:49:46 +0300

    coordinate

---
+++ src/main/java/com/project/common/model/Coordinate.java
@@ -0,0 +1,38 @@
+package com.project.common.model;
+
+/**
+ * @author Egemen
+ *
+ */
+public class Coordinate {
+    private double latitude;
+    private double longitude;
+
+    /**
+     * @param latitude
+     * @param longitude
+     */
+    public Coordinate(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Coordinate() {}
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+}
